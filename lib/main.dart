1da122b5e0366c304f421355d5f97b0324a08a28
Aluno <Aluno@LAB2M24>
2022-03-07 16:37:57 -0300

    dadada

--- lib/main.dart
+++ lib/main.dart
@@ -2,23 +2,25 @@ import 'package:flutter/material.dart';
 
 void main() =>
     runApp(
-      MaterialApp(
+      const MaterialApp(
         home: Home(),
         debugShowCheckedModeBanner: false,
       ),
     );
 
 class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
-  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
-  TextEditingController _weightController = TextEditingController();
-  TextEditingController _heightController = TextEditingController();
-  String _result;
+  final TextEditingController _weightController = TextEditingController();
+  final TextEditingController _heightController = TextEditingController();
+  late String _result;
 
   @override
   void initState() {
@@ -41,18 +43,19 @@ class _HomeState extends State<Home> {
 
     setState(() {
       _result = "IMC = ${imc.toStringAsPrecision(2)}\n";
-      if (imc < 18.6)
+      if (imc < 18.6) {
         _result += "abaixo do peso";
-      else if (imc < 25.0)
+      } else if (imc < 25.0) {
         _result += "peso ideal";
-      else if (imc < 30.0)
+      } else if (imc < 30.0) {
         _result += "Levemente acima do peso";
-      else if (imc < 35.0)
+      } else if (imc < 35.0) {
         _result += "Obesidade Grau I";
-      else if (imc < 40.0)
+      } else if (imc < 40.0) {
         _result += "Obesidade Grau II";
-      else
+      } else {
         _result += "Obesidade Grau III";
+      }
     });
   }
 
@@ -62,16 +65,16 @@ class _HomeState extends State<Home> {
         appBar: buildAppBar(),
         backgroundColor: Colors.white,
         body: SingleChildScrollView(
-            padding: EdgeInsents.all(20.0), child: buildForm()));
+            padding: const EdgeInsets.all(20.0), child: buildForm()));
   }
 
-  AppBar buidAppBar() {
+  AppBar buildAppBar() {
     return AppBar(
-      title: Text('Calculadora de IMC'),
+      title: const Text('Calculadora de IMC'),
       backgroundColor: Colors.blue,
       actions: <Widget>[
         IconButton(
-          icon: Icon(Icons.refresh),
+          icon: const Icon(Icons.refresh),
           onPressed: () {
             resetFields();
           },
@@ -79,3 +82,61 @@ class _HomeState extends State<Home> {
       ],
     );
   }
+
+  Form buildForm() {
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          buildTextFormField(
+              label: "Peso(kg)",
+              error: "Insira seu peso!",
+              controller: _weightController),
+          buildTextFormField(
+            label: "Altura(cm)",
+            error: "Insira uma altura!",
+            controller:_heightController),
+          buildTextResult(),
+          buildCalculateButton(),
+        ],
+      ),
+    );
+  }
+
+  Padding buildCalculateButton() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 36.0),
+      child: ElevatedButton(
+        onPressed: () {
+          if (_formKey.currentState!.validate()) {
+            calculateImc();
+          }
+        },
+        child: const Text('CALCULAR', style: TextStyle(color: Colors.white)),
+      ),
+    );
+  }
+
+  Padding buildTextResult() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 36.0),
+      child: Text(
+        _result,
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+
+  TextFormField buildTextFormField(
+      {required TextEditingController controller, required String error, required String label}) {
+    return TextFormField(
+      keyboardType: TextInputType.number,
+      decoration: InputDecoration(labelText: label),
+      controller: controller,
+      validator: (text){
+    return text!.isEmpty ? error: null;
+    },
+    );
+  }
+}
